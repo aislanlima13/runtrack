a9c42b078506884c48bfb5eaf2660481f1642df5
aislanlima13 <aislan.martins99@gmail.com>
2025-02-22 08:41:27 -0300

    Configura o modulo build-logic

---
+++ build-logic/convention/src/main/java/com/aislan/convention/MyClass.kt
@@ -0,0 +1,4 @@
+package com.aislan.convention
+
+class MyClass {
+}
---
+++ build-logic/settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+    }
+}
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    versionCatalogs {
+        create("libs") {
+            from(files("../gradle/libs.versions.toml"))
+        }
+    }
+}
+
+rootProject.name = "build-logic"
+include(":convention")
