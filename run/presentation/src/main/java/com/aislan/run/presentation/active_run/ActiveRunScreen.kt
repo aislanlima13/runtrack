20c78e8f332f0e6ccb82d25e1930d3fb3ad4cfea
aislanlima13 <aislan.martins99@gmail.com>
2025-05-28 08:07:40 -0300

    Cria o display do Google Maps

--- run/presentation/src/main/java/com/aislan/run/presentation/active_run/ActiveRunScreen.kt
+++ run/presentation/src/main/java/com/aislan/run/presentation/active_run/ActiveRunScreen.kt
@@ -33,6 +33,7 @@ import com.aislan.core.presentation.designsystem.components.RuntrackScaffold
 import com.aislan.core.presentation.designsystem.components.RuntrackToolbar
 import com.aislan.run.presentation.R
 import com.aislan.run.presentation.active_run.components.RunDataCard
+import com.aislan.run.presentation.active_run.maps.TrackerMap
 import com.aislan.run.presentation.util.hasLocationPermission
 import com.aislan.run.presentation.util.hasNotificationPermission
 import com.aislan.run.presentation.util.shouldShowLocationPermissionRationale
@@ -140,6 +141,13 @@ private fun ActiveRunScreen(
                 .fillMaxSize()
                 .background(MaterialTheme.colorScheme.surface)
         ) {
+            TrackerMap(
+                isRunFinished = state.isRunFinished,
+                currentLocation = state.currentLocation,
+                locations = state.runData.locations,
+                onSnapshot = {},
+                modifier = Modifier.fillMaxSize()
+            )
             RunDataCard(
                 elapsedTime = state.elapsedTime,
                 runData = state.runData,
